2053135fb88375240007627c60cf50b1f8ec2697
onlyphantom <samuel@outlook.sg>
2018-08-23 00:13:08 +0700

    'hello world!'

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
+# Creates the application object as an instance of class Flask
 app = Flask(__name__)
 
+# Import at the bottom to workaround circular imports; 
+# Routes module needs to import the app variable defined in 
+# this script, so putting the reciprocal imports at the
+# bottom avoids the error from mutual references between
+# these two files
 from app import routes
---
+++ microblog.py
@@ -0,0 +1,5 @@
+from app import app
+
+# Tell FLASK how to import it to run by
+# setting the environment variable:
+# export FLASK_APP = microblog.py
