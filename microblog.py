f2cda6a4ab0e553dcfb794b9c36ede17b5581d3f
onlyphantom <samuel@outlook.sg>
2018-08-23 00:44:04 +0700

    First Chapter

--- microblog.py
+++ microblog.py
@@ -2,4 +2,6 @@ from app import app
 
 # Tell FLASK how to import it to run by
 # setting the environment variable:
-# export FLASK_APP = microblog.py
+# export FLASK_APP=microblog.py
+# Alternative, set it in .flaskenv
+# then in terminal: flask run
