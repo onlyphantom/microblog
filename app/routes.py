67e816dd36365d1279cfcbd4f3a16e971271d1e0
onlyphantom <samuel@outlook.sg>
2018-08-25 21:21:01 +0700

    Form and flash messages

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import (flash, redirect, render_template, 
+url_for)
 from app import app
 from app.forms import LoginForm
 
@@ -18,8 +19,13 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     # instantiate an object from the LoginForm class
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))    
     return render_template('login.html', title='Sign In', form=form)
