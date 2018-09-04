6f83027f6ec905a4d6b85d7df1a683a84371780c
onlyphantom <samuel@outlook.sg>
2018-09-04 16:12:47 +0700

    Code reformatting + User profile

--- app/models.py
+++ app/models.py
@@ -18,6 +18,8 @@ class User(UserMixin, db.Model):
     # for p in posts:
     #   print(p.id, p.author.username, p.author.email, p.body)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     # __repr__ is a built-in function to compute the official
     # string representation of an object
@@ -33,7 +35,7 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
 from flask import (flash, redirect, render_template,
                    request, url_for)
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 from flask_login import (current_user, login_user, logout_user,
                          login_required)
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -32,12 +39,12 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
+        this_user = User.query.filter_by(username=form.username.data).first()
+        if this_user is None or not this_user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
 
-        login_user(user, remember=form.remember_me.data)
+        login_user(this_user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -73,3 +80,18 @@ def user(username):
         {'author': this_user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=this_user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
