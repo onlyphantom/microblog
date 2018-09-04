2f40dc5111e16e58842b80e27d15f3ea8363a3e5
onlyphantom <samuel@outlook.sg>
2018-09-04 14:42:11 +0700

    Added Gravatar functionality

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 # inherits from db.Model class, a base class for all models
 # from Flask-SQLAlchemy
@@ -32,7 +33,12 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size
+        )
 
 class Post(db.Model):
     """Model for posts.
