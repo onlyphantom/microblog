59cb348cf7c5c4d243569c28479403a80cdbd60e
onlyphantom <samuel@outlook.sg>
2018-09-09 00:09:22 +0700

    Follower Relationship

--- app/models.py
+++ app/models.py
@@ -4,22 +4,38 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+# followers association table
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 # inherits from db.Model class, a base class for all models
 # from Flask-SQLAlchemy
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     # a relationship; posts is not an actual column in the table
     # allow us to do things like:
     # posts = Post.query.all()
     # for p in posts:
     #   print(p.id, p.author.username, p.author.email, p.body)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     # __repr__ is a built-in function to compute the official
     # string representation of an object
@@ -30,6 +46,7 @@ class User(UserMixin, db.Model):
     # >>> u = User(username = 'susan', email='susan@example.com')
     # >>> u
     # <User susan>
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -41,6 +58,26 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size
         )
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            # check followers table, followed_id column
+            followers.c.followed_id == user.id).count() == 1
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
+
 
 class Post(db.Model):
     """Model for posts.
@@ -54,6 +91,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
 
 @login.user_loader
 def load_user(user_id):
--- config.py
+++ config.py
@@ -4,16 +4,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # create the configuration class
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
-    'you-will-never-guess'
+        'you-will-never-guess'
 
-    #sqlite:////Users/Samuel/Dropbox/Projects/Python/Microblog/app.db
+    # sqlite:////Users/Samuel/Dropbox/Projects/Python/Microblog/app.db
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_ECHO = True
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
