1f9f71ee8595757793c2ea619caa33afdd282beb
onlyphantom <samuel@outlook.sg>
2018-09-19 17:50:14 +0700

    Added explore page

--- app/routes.py
+++ app/routes.py
@@ -27,16 +27,7 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    posts = current_user.followed_posts().all()
     return render_template('index.html', title='Home', form=form, posts=posts)
 
 
@@ -141,3 +132,10 @@ def unfollow(username):
     db.session.commit()
     flash('You have unfollowed {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
